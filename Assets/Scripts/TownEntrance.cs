f45e8e69717180ff96f8243a8c0adb798704ef0e
Ron Welch <mmcelt@yahoo.com>
2020-05-15 18:09:30 -0600

    B4 installing Univeral Pipeline

---
+++ Assets/Scripts/TownEntrance.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TownEntrance : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] string _areaToLoad;
+	public string _areaTransitionName, _townName;
+	[SerializeField] AreaEntrance _theEntrance;
+	[SerializeField] float _waitToLoad = 1f;
+	[SerializeField] GameObject _theCanvas;
+	[SerializeField] Text _townNameText;
+
+	bool _shouldLoadAfterFade, _enteredArea;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Awake()
+	{
+		_theEntrance._transitionName = _areaTransitionName;
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (_shouldLoadAfterFade && _enteredArea)
+		{
+			_waitToLoad -= Time.deltaTime;
+		}
+		if (_waitToLoad <= 0)
+		{
+			SceneManager.LoadScene(_areaToLoad);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_townNameText.text = "Welcome to " + _townName;
+			_enteredArea = true;
+			Camera.main.orthographicSize = 5;
+			PlayerController.Instance._areaTransitionName = _areaTransitionName;
+			GameManager.Instance._fadingBetweenAreas = true;
+			_theCanvas.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_enteredArea = false;
+			Camera.main.orthographicSize = 10;
+			GameManager.Instance._fadingBetweenAreas = false;
+			_theCanvas.SetActive(false);
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void OnYesButtonClicked()
+	{
+		_shouldLoadAfterFade = true;
+		UIFade.Instance.FadeToBlack();
+	}
+
+	public void OnNoButtonClicked()
+	{
+		_enteredArea = false;
+		Camera.main.orthographicSize = 10;
+		_theCanvas.SetActive(false);
+		GameManager.Instance._fadingBetweenAreas = false;
+		PlayerController.Instance.transform.position = _theEntrance.transform.position;
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
---
+++ Assets/Scripts/UI/TownInfo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TownInfo : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] string _townName;
+	[SerializeField] GameObject _canvas;
+	[SerializeField] Text _townText;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_townText.text = "Welcome to " + _townName;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_canvas.SetActive(true);
+			Camera.main.orthographicSize = 5;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_canvas.SetActive(false);
+			Camera.main.orthographicSize = 10;
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void OnYesButtonClicked()
+	{
+
+	}
+
+	public void OnNoButtonClicked()
+	{
+		_canvas.SetActive(false);
+		Camera.main.orthographicSize = 10;
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
