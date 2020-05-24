ffdf35c1697d294ff528a8da822e8bbe68875e10
Ron Welch <mmcelt@yahoo.com>
2020-05-24 16:36:56 -0600

    More images and worked on world map a little

---
+++ Assets/Asset Packs/2D Pixel RPG icon pack/Script/PutItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PutItem : MonoBehaviour
+{
+    public List<Sprite> ItemList = new List<Sprite>();
+    public Image Item;
+
+    void Start()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        Item.sprite = ItemList[Random.Range(0, 377)];
+    }
+}
